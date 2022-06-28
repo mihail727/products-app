455f033729f5b9f2eaaf3b3d9bf62d8774af2d13
mihail727 <suppnms@gmail.com>
2022-06-28 21:30:45 +0300

    feat!: added meta config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,10 @@
 import { defineNuxtConfig } from 'nuxt';
 import modulesConfig from './configs/nuxt.modules.config';
+import metaConfig from './configs/nuxt.meta.config';
 
 export default defineNuxtConfig({
 	...modulesConfig,
+	...metaConfig,
 	nitro: {
 		output: {
 			dir: '~~/build',
