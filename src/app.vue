b662ef108341263e78b44aade137300a3f6eaeb1
mihail727 <suppnms@gmail.com>
2022-06-28 21:32:15 +0300

    refactor

--- src/app.vue
+++ src/app.vue
@@ -1,7 +1,5 @@
 <template>
-	<div class="min-h-screen min-w-screen">
-		<NuxtLayout>
-			<NuxtPage />
-		</NuxtLayout>
+	<div class="min-h-screen min-w-screen bg-[#E5E5E5]">
+		<NuxtPage />
 	</div>
 </template>
