92acab2efed4ed1bbca99d6f2718fdf6e2884637
mihail727 <suppnms@gmail.com>
2022-06-28 21:32:42 +0300

    feat: added product add form

---
+++ src/components/VProductAddForm.vue
@@ -0,0 +1,55 @@
+<script setup></script>
+
+<template>
+	<div
+		class="flex flex-col space-y-[16px] w-[332px] p-[24px] bg-[#FFFEFB] rounded shadow-xl text-[#49485E]"
+	>
+		<div class="flex flex-col space-y-1">
+			<h3 class="text-[10px] after:content-['*'] after:text-[#FF8484]">
+				Наименование товара
+			</h3>
+			<input
+				type="text"
+				class="rounded shadow-md text-[12px] h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px] focus:border-none"
+				placeholder="Введите наименование товара"
+			/>
+		</div>
+
+		<div class="flex flex-col space-y-1">
+			<h3 class="text-[10px]">Описание товара</h3>
+			<textarea
+				class="rounded shadow-md text-[12px] placeholder:text-[#B4B4B4] p-[10px_16px] resize-none"
+				placeholder="Введите описание товара"
+				cols="30"
+				rows="6"
+			></textarea>
+		</div>
+
+		<div class="flex flex-col space-y-1">
+			<h3 class="text-[10px] after:content-['*'] after:text-[#FF8484]">
+				Ссылка на изображение товара
+			</h3>
+			<input
+				type="text"
+				class="rounded shadow-md text-[12px] h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px]"
+				placeholder="Введите ссылку"
+			/>
+		</div>
+
+		<div class="flex flex-col space-y-1">
+			<h3 class="text-[10px] after:content-['*'] after:text-[#FF8484]">Цена товара</h3>
+			<input
+				type="text"
+				class="rounded shadow-md text-[12px] h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px]"
+				placeholder="Введите цену"
+			/>
+		</div>
+
+		<button
+			disabled
+			class="disabled:bg-[#EEEEEE] disabled:text-[#B4B4B4] rounded-lg p-[10px] text-[12px]"
+		>
+			Добавить товар
+		</button>
+	</div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,10 @@
-<script setup lang="ts"></script>
+<script setup></script>
 
 <template>
-	<div>index page</div>
+	<div class="grid grid-cols-[30%_70%] w-full min-h-screen p-[32px]">
+		<div class="flex flex-col space-y-[16px]">
+			<h2 class="text-[28px] text-[#3F3F3F] font-[600] leading-[35px]">Добавление товара</h2>
+			<VProductAddForm />
+		</div>
+	</div>
 </template>
