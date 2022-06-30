6fe3a722072e706a35a369d8ebdc0ab6bb20af17
mihail727 <suppnms@gmail.com>
2022-06-30 13:14:59 +0300

    refactor(style)

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,7 +3,7 @@
 <template>
 	<div class="grid grid-cols-[30%_70%] w-full min-h-screen p-[32px]">
 		<div class="flex flex-col space-y-[16px]">
-			<h2 class="text-[28px] text-[#3F3F3F] font-[600] leading-[35px]">Добавление товара</h2>
+			<h2 class="text-[28px] text-[#3F3F3F] font-[600] ">Добавление товара</h2>
 			<VProductAddForm />
 		</div>
 	</div>
