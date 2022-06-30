5920d6ea13f4ae2fffbb52246bdc08a8f9f84d3f
mihail727 <suppnms@gmail.com>
2022-06-30 13:15:11 +0300

    refactor(style)

--- src/components/VProductAddForm.vue
+++ src/components/VProductAddForm.vue
@@ -1,24 +1,35 @@
-<script setup></script>
+<script setup lang="ts">
+	const productName = ref('');
+	const productDesc = ref('');
+	const imageLink = ref('');
+	const productPrice = ref<string>();
+
+	const onPriceInputKeyUp = (event: Event) => {
+		const el = event.currentTarget as HTMLInputElement;
+		let value = el.value.split(' ').join('');
+		el.value = usePriceFormat(value);
+	};
+</script>
 
 <template>
 	<div
 		class="flex flex-col space-y-[16px] w-[332px] p-[24px] bg-[#FFFEFB] rounded shadow-xl text-[#49485E]"
 	>
 		<div class="flex flex-col space-y-1">
-			<h3 class="text-[10px] after:content-['*'] after:text-[#FF8484]">
-				Наименование товара
-			</h3>
+			<h3 class="text-xs after:content-['*'] after:text-[#FF8484]">Наименование товара</h3>
 			<input
+				v-model="productName"
 				type="text"
-				class="rounded shadow-md text-[12px] h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px] focus:border-none"
+				class="rounded shadow-md text-sm h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px] focus:border-none"
 				placeholder="Введите наименование товара"
 			/>
 		</div>
 
 		<div class="flex flex-col space-y-1">
-			<h3 class="text-[10px]">Описание товара</h3>
+			<h3 class="text-xs">Описание товара</h3>
 			<textarea
-				class="rounded shadow-md text-[12px] placeholder:text-[#B4B4B4] p-[10px_16px] resize-none"
+				v-model="productDesc"
+				class="rounded shadow-md text-sm placeholder:text-[#B4B4B4] p-[10px_16px] resize-none"
 				placeholder="Введите описание товара"
 				cols="30"
 				rows="6"
@@ -26,28 +37,31 @@
 		</div>
 
 		<div class="flex flex-col space-y-1">
-			<h3 class="text-[10px] after:content-['*'] after:text-[#FF8484]">
+			<h3 class="text-xs after:content-['*'] after:text-[#FF8484]">
 				Ссылка на изображение товара
 			</h3>
 			<input
+				v-model="imageLink"
 				type="text"
-				class="rounded shadow-md text-[12px] h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px]"
+				class="rounded shadow-md text-sm h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px]"
 				placeholder="Введите ссылку"
 			/>
 		</div>
 
 		<div class="flex flex-col space-y-1">
-			<h3 class="text-[10px] after:content-['*'] after:text-[#FF8484]">Цена товара</h3>
+			<h3 class="text-xs after:content-['*'] after:text-[#FF8484]">Цена товара</h3>
 			<input
+				v-model="productPrice"
+				@keyup="onPriceInputKeyUp"
 				type="text"
-				class="rounded shadow-md text-[12px] h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px]"
+				class="rounded shadow-md text-sm h-[36px] placeholder:text-[#B4B4B4] p-[10px_16px]"
 				placeholder="Введите цену"
 			/>
 		</div>
 
 		<button
 			disabled
-			class="disabled:bg-[#EEEEEE] disabled:text-[#B4B4B4] rounded-lg p-[10px] text-[12px]"
+			class="disabled:bg-[#EEEEEE] disabled:text-[#B4B4B4] rounded-lg p-[10px] text-sm"
 		>
 			Добавить товар
 		</button>
