0d00dbd293087acf382425549cf97fd26f7f3a42
mihail727 <suppnms@gmail.com>
2022-07-01 16:15:34 +0300

    refactor: split to 2 functions

--- src/composables/usePriceFormat.ts
+++ src/composables/usePriceFormat.ts
@@ -1,3 +1,11 @@
-export default function (value: string | number): string {
-	return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/gm, '$1 ');
+export default function () {
+	const formatForView = (value: number | string): string => {
+		return value.toString().replace(/(\d)(?=(\d{3})+(?!\d))/gm, '$1 ');
+	};
+
+	const formatForData = (value: string): number => {
+		return parseInt(value.split(' ').join(''));
+	};
+
+	return { formatForData, formatForView };
 }
