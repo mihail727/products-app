d116fc249e2b37fd2aad9a2ea9c8c701ad0302a6
mihail727 <suppnms@gmail.com>
2022-07-02 19:48:22 +0300

    refactor: ref value of array

--- src/composables/useProduct.ts
+++ src/composables/useProduct.ts
@@ -1,5 +1,8 @@
+import { promiseTimeout } from '@vueuse/core';
+
 export default () => {
 	const productList = useState<IProduct[]>('product-list', () => null);
+	const pending = ref(false);
 
 	const setup = async () => {
 		const list = useLocalStorage<IProduct[]>('product-list', null, {
@@ -16,43 +19,62 @@ export default () => {
 		productList.value = list.value;
 	};
 
-	const removeById = (id: number) => {
-		productList.value.splice(id, 1);
+	const load = async () => {
+		pending.value = true;
+		await promiseTimeout(800);
+		pending.value = false;
+		return productList.value.map((v) => v);
 	};
 
-	return { productList, setup, removeById };
+	const add = (newProduct: IProduct) => {
+		newProduct.id = productList.value.length + 1;
+		productList.value.splice(productList.value.length, 0, newProduct);
+		return productList.value.map((v) => v);
+	};
+
+	const removeById = (id: number) => {
+		productList.value.splice(id, 1);
+		return productList.value.map((v) => v);
+	};
+
+	return { setup, removeById, load, add, pending };
 };
 
 function getInitialProducts(): IProduct[] {
 	return [
 		{
-			name: 'Наименование товара',
+			id: 1,
+			name: 'Наименование товара 1',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			price: 10000,
+			price: 110000,
 			imageLink: 'https://i.imgur.com/g6xw8wT.png',
 		},
 		{
-			name: 'Наименование товара',
+			id: 2,
+			name: 'Наименование товара 2',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			price: 10000,
+			price: 12000,
 			imageLink: 'https://i.imgur.com/g6xw8wT.png',
 		},
 		{
-			name: 'Наименование товара',
+			id: 3,
+			name: 'Наименование товара 3',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			price: 10000,
+			price: 1000,
 			imageLink: 'https://i.imgur.com/g6xw8wT.png',
 		},
 		{
-			name: 'Наименование товара',
+			id: 4,
+			name: 'Наименование товара 4',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			price: 10000,
+			price: 10300,
 			imageLink: 'https://i.imgur.com/g6xw8wT.png',
 		},
 		{
-			name: 'Наименование товара',
+			id: 5,
+			name: 'Наименование товара 5',
 			desc: 'Довольно-таки интересное описание товара в несколько строк. Довольно-таки интересное описание товара в несколько строк',
-			price: 10000,
+			price: 10350,
 			imageLink: 'https://i.imgur.com/g6xw8wT.png',
 		},
 	];
